ee9aae0be35f87b0548143e5b3e236a401cb62b1
Mostafa <m.alsham627@gmail.com>
2020-11-17 14:08:14 -0700

    abc

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//First API: NHTSA
+// First API: NHTSA
 // API Key not needed
 // database URL: https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvaluesextended/{VIN number}format=json&modelyear={year}
 
-//Second API: Unsplash
-//API Key: pBuNE2iE6rxSCwu9y9mrpwI0NdVcSm3YwKtL-cpO5eg
+// Second API: Unsplash
+// API Key: pBuNE2iE6rxSCwu9y9mrpwI0NdVcSm3YwKtL-cpO5eg
 // database URL: https://api.unsplash.com/search/photos?page=1&query={KEYWORD}&client_id={API_KEY}
 
