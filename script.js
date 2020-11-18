2ed5606205b44b965cc98b1259fc0166c97b3bd6
Mostafa <m.alsham627@gmail.com>
2020-11-18 13:18:41 -0700

    added AJAX call for NHTSA

--- script.js
+++ script.js
@@ -4,11 +4,22 @@
 // variables needed : Vin number and year from user.
 // format year to be numbers only.
 var vinNumber;
-var year;
+var year = $("");
 
-const statsQueryURL = `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvaluesextended/${vinNumber}format=json&modelyear=${year}`
+const statsQueryURL = `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvaluesextended/${vinNumber}?format=json&modelyear=${year}`
 
-
+//AJAX call for car stats from NHTSA
+$.ajax({
+    method: "GET",
+    url: statsQueryURL,
+}).then(function (response) {
+    var data = response.results
+    $("#make-id").text("Make : " + data.Make);
+    $("#model-id").text("Model : " +data.Model);
+    $("#engine-size").text("Engine size: " + data.DisplacementL + "L");
+    $("#engine-cylinders").text("Engine cylinders: " + data.EngineCylinders);
+    $("#engine-hp").text("Horsepower: " + data.EngineHP);
+})
 // Second API: Unsplash
 // API Key: pBuNE2iE6rxSCwu9y9mrpwI0NdVcSm3YwKtL-cpO5eg
 // database URL: https://api.unsplash.com/search/photos?page=1&query={KEYWORD}&client_id={API_KEY}
@@ -16,3 +27,8 @@ var keyWord;    //get user input val()
 var apiKey = "pBuNE2iE6rxSCwu9y9mrpwI0NdVcSm3YwKtL-cpO5eg"
 
 const imageQueryURL = `https://api.unsplash.com/search/photos?page=1&query=${keyWord}&client_id=${apiKey}`
+
+//key features: Make, Model, BodyClass, DisplacementL, EngineCylinders, EngineHP
+
+
+//ajax call for 
